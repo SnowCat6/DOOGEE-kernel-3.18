115b14ef53b384f8ef5b0bcb7b3c6024d1adda91
SnowCat6 <SnowCat@vpro.ru>
2016-08-20 02:41:34 +0500

    UPDATE 1

--- drivers/base/power/main.c
+++ drivers/base/power/main.c
@@ -385,7 +385,7 @@ static void dpm_show_time(ktime_t starttime, pm_message_t state, char *info)
 	usecs = usecs64;
 	if (usecs == 0)
 		usecs = 1;
-	hib_log("PM: %s%s%s of devices complete after %ld.%03ld msecs\n",
+	pr_info("PM: %s%s%s of devices complete after %ld.%03ld msecs\n",
 		info ?: "", info ? " " : "", pm_verb(state.event),
 		usecs / USEC_PER_MSEC, usecs % USEC_PER_MSEC);
 }
